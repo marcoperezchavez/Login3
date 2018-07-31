5b6976980f22d90dd4fdce9ef5788419477dd4d7
Marco Perez <mperez@anexinet.com>
2018-07-30 22:12:24 -0500

    Menu: Added css and html layout

--- LoginSecu/ClientApp/src/app/Menu/Menu.component.ts
+++ LoginSecu/ClientApp/src/app/Menu/Menu.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'menu-home',
   templateUrl: './Menu.component.html',
+  styleUrls: ['./Menu.component.css']
 })
 export class MenuComponent {
 }
