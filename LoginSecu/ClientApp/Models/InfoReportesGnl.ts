cdd0de7a035bcd98f7823fdf2d340b3b8de907d8
Marco Perez <mperez@anexinet.com>
2018-08-23 22:38:20 -0500

    insertar-reportes: funcionalidad completa en la UI

---
+++ LoginSecu/ClientApp/Models/InfoReportesGnl.ts
@@ -0,0 +1,11 @@
+ export class InfoReportesGnl
+  {  
+   id: number;
+   idAlumno: number;
+   idReporte: number;
+   nombreReporte: string;
+   description: string;
+   creationDate: Date;
+   totalDays: number;
+}
+
--- LoginSecu/ClientApp/src/app/insertar-reporte/insertar-reporte.component.ts
+++ LoginSecu/ClientApp/src/app/insertar-reporte/insertar-reporte.component.ts
@@ -1,8 +1,9 @@
 /// <reference path="../../../models/alumnadognl.ts" />
+/// <reference path="../../../models/inforeportesgnl.ts" />
 import { Component, OnInit, ViewChild, AfterContentInit } from '@angular/core';
 import { AlumnadoGnl } from "../../../Models/AlumnadoGnl";
 import { SeleccionarComponent } from "../seleccionar/seleccionar.component";
-
+import { InfoReportesGnl } from "../../../models/inforeportesgnl";
 
 @Component({
   selector: 'app-insertar-reporte',
@@ -16,6 +17,7 @@ export class InsertarReporteComponent implements OnInit {
 
   //alumno: AlumnadoGnl;
   alumn = new AlumnadoGnl();
+  reporte: InfoReportesGnl = null;
   public id: number;
   public idTest: number;
 
@@ -33,13 +35,17 @@ export class InsertarReporteComponent implements OnInit {
   }
 
   ngOnInit() {
-
+    this.reporte = new InfoReportesGnl();
 
     //let alumnotest = localStorage.getItem("alumnoLS");
     //this.alumno = JSON.parse(alumnotest);
     localStorage.clear();
     console.log(this.alumno);
     
+  }
+
+  IngresarReporte() {
+    console.log(this.reporte);
   }
 
 }
